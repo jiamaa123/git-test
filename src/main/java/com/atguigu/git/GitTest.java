bb776c409d19fd3bb9ba0d9b4c4adb5d5767c7ba
jiamaa123 <99522896+jiamaa123@users.noreply.github.com>
2022-05-31 17:15:52 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("hot-fix pushtest");
         System.out.println("hot-fix pushtest1");
+        System.out.println("hot-fix pulltest1");
     }
 }
