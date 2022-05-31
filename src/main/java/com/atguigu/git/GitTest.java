438b6da5edb81ee32d0d4e64d17b61f0107b4424
jiamaa <jiamaa@atguigu.com>
2022-05-31 16:19:05 +0800

    master forth commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github1");
         System.out.println("hello github4");
+        System.out.println("master-test github4");
+
     }
 }
