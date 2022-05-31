e35bcef9d08d5b4d22f08d7a735425e4ebc09eb5
jiamaa <jiamaa@atguigu.com>
2022-05-31 16:33:30 +0800

    cc firstcommit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github1");
         System.out.println("hello github4");
-        System.out.println("master-test github4");
+        System.out.println("master-test github45");
 
         System.out.println("hot-fix test");
     }
