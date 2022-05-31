5f53582b7a8a02295c9fd382b64b881192d57788
jiamaa <jiamaa@atguigu.com>
2022-05-31 16:12:38 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello github");
+        System.out.println("hello github1");
         System.out.println("hello github4");
     }
 }
